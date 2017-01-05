6cb825470c9fab7c48f509f1be45888bc8a2ac2d
Bright <9034785@qq.com>
2017-01-05 23:56:24 +0800

    Function update
    1. hop by hop print
    2. calculate the AS border and print ‘=‘

--- trace_as.py
+++ trace_as.py
@@ -7,65 +7,79 @@ import urllib.request
 import time
 
 
-def get_ip_list(target):
-    ip_list = []
-    trace_list = subprocess.check_output(['traceroute', '-nI', '-q', '1', '-w', '1', target]).decode().split('\n')
-    for i in trace_list[:-1]:
-        if len(i) < 10:
-            ip_list.append('*')
-        else:
-            ip_list.append(re.search(r'(\d+\.\d+\.\d+\.\d+)', i).group(1))
-    return ip_list
+def search_as_name_merit(ip):
+    name_tuple = subprocess.Popen(['whois', '-h', '198.108.0.18', ip], stdout=subprocess.PIPE)
+    name_raw = name_tuple.communicate()[0].decode()
+    if 'No entries found' in name_raw:
+        result = 'Not Found'
+        asn = 'Not Found'
+    else:
+        result = re.search(r'descr:\s+(.+)', name_raw).group(1)
+        asn = re.search(r'origin:\s+(.+)', name_raw).group(1)
+    return result, asn
 
 
-def search_as_name_merit(ip_list):
-    name_list = []
-    for i in ip_list:
-        if i is '*':
-            name_list.append('*')
-        else:
-            name_raw = subprocess.check_output(['whois', '-h', '198.108.0.18', i]).decode().split('\n')
-            if len(name_raw) <= 2:
-                name_list.append('Not Found')
-            else:
-                name = re.match(r'descr:\s+(.+)', name_raw[1]).group(1)
-                name_list.append(name)
-    return name_list
+def search_as_name_cymru(ip):
+    name_tuple = subprocess.Popen(['whois', '-h', 'v4.whois.cymru.com', ip], stdout=subprocess.PIPE)
+    name_raw = name_tuple.communicate()[0].decode().split('\n')
+    result = re.search(r'\d+\.\d+\.\d+\.\d+\s+\|\s(.+)', name_raw[1]).group(1)
+    return result
 
 
-def search_as_name_cymru(ip_list):
-    name_list = []
-    for i in ip_list:
-        if i is '*':
-            name_list.append('*')
-        else:
-            name_raw = subprocess.check_output(['whois', '-h', 'v4.whois.cymru.com', i]).decode().split('\n')
-            name = re.search(r'\d+\.\d+\.\d+\.\d+\s+\|\s(.+)', name_raw[1]).group(1)
-            name_list.append(name)
-    return name_list
-
-
-def search_as_name_ipip(ip_list):
-    name_list = []
-    for i in ip_list:
-        if i is '*':
-            name_list.append('*')
-        else:
-            name_raw_str = urllib.request.urlopen("http://freeapi.ipip.net/" + i).read().decode()
-            time.sleep(5)
-            name_list.append(name_raw_str[1:-1])
-    return name_list
+def search_as_name_ipip(ip):
+    name_raw_str = urllib.request.urlopen("http://freeapi.ipip.net/" + ip).read().decode()
+    time.sleep(1)
+    result = name_raw_str[1:-1]
+    return result
 
 
 def main():
-    ip_list = get_ip_list(input("Please enter the target IP address to trace: "))
-    name_list_merit = search_as_name_merit(ip_list)
-    name_list_cymru = search_as_name_cymru(ip_list)
-    name_list_ipip = search_as_name_ipip(ip_list)
+    target = input("Please enter the IP address to trace: ")
+    max_ttl = input("Please enter the max hops: ")
+    raw_list = []
+    last_asn = ''
+    print("IP Address".ljust(18) + "ASN".ljust(13) + "MeritRADb".ljust(30) + "Team Cymru".ljust(30) + "ipip.net")
 
-    print("Every Hop".ljust(20) + "MeritRADb".ljust(50) + "Team Cymru".ljust(50) + "ipip.net")
-    for ip,merit, cymru, ipip in zip(ip_list, name_list_merit, name_list_cymru, name_list_ipip):
-        print(ip.ljust(20) + merit.ljust(50) + cymru.ljust(50) + ipip)
+    for i in range(1, int(max_ttl)+1):
+        raw_output = subprocess.Popen(['ping', '-c', '1', '-m', str(i), '-i', '0.1', '-t', '1', target],
+                                      stdout=subprocess.PIPE)
+        output = raw_output.communicate()[0].decode()
+
+        # if mtu expired, the remote end will send our Time to live exceeded ICMP error message, the error message
+        # contain the IP address of the responding (hop),also we capture the (ip) address if the target is a domain name
+        if 'Time to live exceeded' in output:
+            output_list = output.split('\n')
+            ip = re.search(r'(\d+\.\d+\.\d+\.\d+)', output_list[0]).group(1)
+            hop = re.search(r'(\d+\.\d+\.\d+\.\d+)', output_list[1]).group(1)
+
+            # To search merit's whois server
+            merit, asn = search_as_name_merit(hop)
+
+            # To calculate the cross AS border and print '='
+            if asn != 'Not Found' and asn != last_asn and last_asn != '':
+                print('=' * 130)
+                last_asn = asn
+            elif asn == 'Not Found' or asn == '*':
+                pass
+            else:
+                last_asn = asn
+
+            # To search Team Cymru's whois server
+            cymru = search_as_name_cymru(hop)
+
+            # To search ipip.net's AS info
+            ipip = search_as_name_ipip(hop)
+
+            # Print the result and cut only the first 30 characters for merit and cymru
+            print(hop.ljust(18) + asn.ljust(13) + merit[:29].ljust(30) + cymru[:29].ljust(30) + ipip)
+        elif '1 packets received' in output:
+            merit, asn = search_as_name_merit(ip)
+            cymru = search_as_name_cymru(ip)
+            ipip = search_as_name_ipip(ip)
+            print(ip.ljust(18) + asn.ljust(13) + merit.ljust[:29].ljust(30) + cymru[:29].ljust(30) + ipip)
+            break
+        else:
+            print('*'.ljust(18) + '*'.ljust(13) + '*'.ljust(30) + '*'.ljust(30) + '*')
 
 
 if __name__ == '__main__':
