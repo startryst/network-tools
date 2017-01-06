49dcdbb1017bc975af680a4dc62868b4bb621b37
Bright <9034785@qq.com>
2017-01-06 14:52:52 +0800

    Update in detail
    1. try/except to see cymru will timeout and reset connection
    2. add print_save function to send output both console and save to file
    3. add as_border_check as function
    4. to make the main() function simplifier to call each sub-function

--- trace_as.py
+++ trace_as.py
@@ -23,7 +23,10 @@ def search_as_name_merit(ip):
 def search_as_name_cymru(ip):
     name_tuple = subprocess.Popen(['whois', '-h', 'v4.whois.cymru.com', ip], stdout=subprocess.PIPE)
     name_raw = name_tuple.communicate()[0].decode().split('\n')
-    result = re.search(r'\d+\.\d+\.\d+\.\d+\s+\|\s(.+)', name_raw[1]).group(1)
+    try:
+        result = re.search(r'\d+\.\d+\.\d+\.\d+\s+\|\s(.+)', name_raw[1]).group(1)
+    except IndexError:
+        result = 'Timeout'
     return result
 
 
@@ -36,67 +39,76 @@ def search_as_name_ipip(ip):
     return result
 
 
+def print_save(hop, asn, merit, cymru, ipip, filename, ip):
+    if hop == "IP Address":
+        template = hop.ljust(15) + '|' + asn.ljust(9) + '|' + merit.ljust(29) + '|' + cymru.ljust(37) + '|' + ipip
+        print('\nTracing to {}: '.format(ip))
+        print(template)
+        with open(filename, "a") as file_to_save:
+            file_to_save.write('\n\n\nTracing to {}: '.format(ip))
+    else:
+        template = hop.ljust(15) + '|' + asn.ljust(9) + '|' + merit[:28].ljust(29) + '|' + cymru[:36].ljust(37)\
+                   + '|' + ipip
+        print(template)
+    with open(filename, "a") as file_to_save:
+        file_to_save.write('\n' + template)
+
+
+def as_border_check(asn, last_asn):
+    if 'AS' in asn and 'AS' in last_asn and asn != last_asn:
+        print('======AS BORDER======' * 6)
+        return asn
+    elif asn == 'Not Found' or asn == '*':
+        return last_asn
+    else:
+        return asn
+
+
 def main():
     target = input("Please enter the domain name/IP address to trace: ")
     max_ttl = input("Please enter the max hops: ")
-    last_asn = ''
-    print("IP Address".ljust(15) + '|' + "ASN".ljust(9) + '|' + "MeritRADb".ljust(29) + '|' + "Team Cymru".ljust(37)
-          + '|' + "ipip.net")
+    file_name = str(input('Please enter the file name to save for the trace result[asn_trace.txt]: ') or
+                    'asn_trace.txt')
 
-    # # capture the (ip) address if the target is a domain name
+    # capture the (ip) address if the target is a domain name
     ip = socket.getaddrinfo(target, None)[0][4][0]
 
+    # print title column
+    print_save("IP Address", "ASN", "MeritRADb", "Team Cymru", "ipip.net", file_name, ip)
+
+    # last_asn for as border check
+    last_asn = ''
     for i in range(1, int(max_ttl)+1):
+
+        # if time-to-exceed error message or echo-reply message received, the console output on 2nd line(output_list[1])
+        # always have IP address of the responding hop
         raw_output = subprocess.Popen(['ping', '-c', '1', '-m', str(i), '-i', '0.1', '-t', '1', ip],
                                       stdout=subprocess.PIPE)
         output = raw_output.communicate()[0].decode()
+        output_list = output.split('\n')
+        try:
 
-        # if mtu expired, the remote end will send our Time to live exceeded ICMP error message, the error message
-        # contain the IP address of the responding (hop)
-        if 'Time to live exceeded' in output:
-            output_list = output.split('\n')
+            # capture the IP address of the responding hop by reg
             hop = re.search(r'(\d+\.\d+\.\d+\.\d+)', output_list[1]).group(1)
 
-            # To search merit's whois server
-            merit, asn = search_as_name_merit(hop)
+        # if not replies from certain hop, then output_list[1] doesn't have any ip address to trigger AttributeError
+        except AttributeError:
+            print_save('*', '*', '*', '*', '*', file_name, ip)
 
-            # To calculate the cross AS border and print '='
-            if asn != 'Not Found' and asn != last_asn and last_asn != '':
-                print('======AS BORDER======' * 6)
-                last_asn = asn
-            elif asn == 'Not Found' or asn == '*':
-                pass
-            else:
-                last_asn = asn
+        # To search merit and cymru's whois server, REST API to search ipip's ASN database
+        merit, asn = search_as_name_merit(hop)
+        cymru = search_as_name_cymru(hop)
+        ipip = search_as_name_ipip(hop)
 
-            # To search Team Cymru's whois server
-            cymru = search_as_name_cymru(hop)
+        # To calculate the cross AS border and print '======AS BORDER======'
+        last_asn = as_border_check(asn, last_asn)
 
-            # To search ipip.net's AS info
-            ipip = search_as_name_ipip(hop)
+        # Print the result and cut only the first 30 characters for merit and cymru
+        print_save(hop, asn, merit, cymru, ipip, file_name, ip)
 
-            # Print the result and cut only the first 30 characters for merit and cymru
-            print(hop.ljust(15) + '|' + asn.ljust(9) + '|' + merit[:28].ljust(29) + '|' + cymru[:36].ljust(37) + '|'
-                  + ipip)
-        elif '1 packets received' in output:
-            merit, asn = search_as_name_merit(ip)
-            cymru = search_as_name_cymru(ip)
-            ipip = search_as_name_ipip(ip)
-
-            # To calculate the cross AS border and print '='
-            if asn != 'Not Found' and asn != last_asn and last_asn != '':
-                print('======AS BORDER======' * 6)
-                last_asn = asn
-            elif asn == 'Not Found' or asn == '*':
-                pass
-            else:
-                last_asn = asn
-
-            print(ip.ljust(15) + '|' + asn.ljust(9) + '|' + merit[:28].ljust(29) + '|' + cymru[:36].ljust(37) + '|'
-                  + ipip)
+        # if echo-reply message received, break out the loop
+        if '1 packets received' in output:
             break
-        else:
-            print('*'.ljust(15) + '|' + '*'.ljust(9) + '|' + '*'.ljust(29) + '|' + '*'.ljust(37) + '|' + '*')
 
 
 if __name__ == '__main__':
