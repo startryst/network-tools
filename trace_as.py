32735a7d52c76fe25621b7418c7b9104f462d803
Bright <9034785@qq.com>
2017-01-06 17:14:06 +0800

    Enable multiprocessing
    1. Enable multiprocessing for merit and cymru whois server, which is
    much faster than before
    2. But due to ipip.net set concurrent connection detection, we have to
    pause 1s for each query
    3. fix bug: save ===AS BODER== to file
    4. for merit and cymru, if the remote end doesn’t respond and reset the
    TCP connection, will return ‘Timeout’ to user

--- trace_as.py
+++ trace_as.py
@@ -5,34 +5,41 @@ import subprocess
 import re
 import urllib.request
 import socket
-# import time
+from fast import fast_trace
+import time
 
 
-def search_as_name_merit(ip):
-    name_tuple = subprocess.Popen(['whois', '-h', '198.108.0.18', ip], stdout=subprocess.PIPE)
-    name_raw = name_tuple.communicate()[0].decode()
-    if 'No entries found' in name_raw:
-        result = 'Not Found'
+def merit_filter(item):
+    if 'No entries found' in item:
+        as_name = 'Not Found'
         asn = 'Not Found'
     else:
-        result = re.search(r'descr:\s+(.+)', name_raw).group(1)
-        asn = re.search(r'origin:\s+(.+)', name_raw).group(1)
-    return result, asn
+        try:
+            as_name = re.search(r'descr:\s+(.+)', item).group(1)
+            asn = re.search(r'origin:\s+(.+)', item).group(1)
+
+        # if whois server no return or reset the TCP connection
+        except AttributeError:
+            as_name = 'Timeout'
+            asn = 'Timeout'
+    return as_name, asn
 
 
-def search_as_name_cymru(ip):
-    name_tuple = subprocess.Popen(['whois', '-h', 'v4.whois.cymru.com', ip], stdout=subprocess.PIPE)
-    name_raw = name_tuple.communicate()[0].decode().split('\n')
+def cymru_filter(item):
     try:
-        result = re.search(r'\d+\.\d+\.\d+\.\d+\s+\|\s(.+)', name_raw[1]).group(1)
-    except IndexError:
-        result = 'Timeout'
-    return result
+        as_name = re.search(r'\d+\.\d+\.\d+\.\d+\s+\|\s(.+)', item).group(1)
+
+    # if whois server no return or reset the TCP connection
+    except AttributeError:
+        as_name = 'Timeout'
+    return as_name
 
 
 def search_as_name_ipip(ip):
     name_raw_str = urllib.request.urlopen("http://freeapi.ipip.net/" + ip).read().decode()
-    # time.sleep(1)
+
+    # ipip.net set con-current connection limit for freeapi, so need to pause for 1 second
+    time.sleep(1)
     raw_string = name_raw_str[1:-1].replace('"','')
     raw_list = raw_string.split(',')
     result = raw_list[1] + ':' + raw_list[-1]
@@ -41,24 +48,26 @@ def search_as_name_ipip(ip):
 
 def print_save(hop, asn, merit, cymru, ipip, filename, ip):
     if hop == "IP Address":
-        template = hop.ljust(15) + '|' + asn.ljust(9) + '|' + merit.ljust(29) + '|' + cymru.ljust(37) + '|' + ipip
+        template = hop.ljust(16) + '|' + asn.ljust(9) + '|' + merit.ljust(29) + '|' + cymru.ljust(37) + '|' + ipip
         print('\nTracing to {}: '.format(ip))
         print(template)
         with open(filename, "a") as file_to_save:
             file_to_save.write('\n\n\nTracing to {}: '.format(ip))
     else:
-        template = hop.ljust(15) + '|' + asn.ljust(9) + '|' + merit[:28].ljust(29) + '|' + cymru[:36].ljust(37)\
+        template = hop.ljust(16) + '|' + asn.ljust(9) + '|' + merit[:28].ljust(29) + '|' + cymru[:36].ljust(37)\
                    + '|' + ipip
         print(template)
     with open(filename, "a") as file_to_save:
         file_to_save.write('\n' + template)
 
 
-def as_border_check(asn, last_asn):
+def as_border_check(asn, last_asn, filename):
     if 'AS' in asn and 'AS' in last_asn and asn != last_asn:
         print('======AS BORDER======' * 6)
+        with open(filename, "a") as file_to_save:
+            file_to_save.write('\n======AS BORDER======' * 6)
         return asn
-    elif asn == 'Not Found' or asn == '*':
+    elif asn == 'Not Found' or asn == '*' or asn == 'Timeout':
         return last_asn
     else:
         return asn
@@ -69,6 +78,10 @@ def main():
     max_ttl = input("Please enter the max hops: ")
     file_name = str(input('Please enter the file name to save for the trace result[asn_trace.txt]: ') or
                     'asn_trace.txt')
+    merit_raw = []
+    cymru_raw = []
+    # last_asn for as border check
+    last_asn = ''
 
     # capture the (ip) address if the target is a domain name
     ip = socket.getaddrinfo(target, None)[0][4][0]
@@ -76,39 +89,38 @@ def main():
     # print title column
     print_save("IP Address", "ASN", "MeritRADb", "Team Cymru", "ipip.net", file_name, ip)
 
-    # last_asn for as border check
-    last_asn = ''
-    for i in range(1, int(max_ttl)+1):
+    # Conduct fast trace twice to get the full hop ip address list, in case one test may miss some hops
+    trace_list = fast_trace(ip, max_ttl)
+    trace_list1 = fast_trace(ip, max_ttl)
+    for t, t1, i in zip(trace_list, trace_list1, range(len(trace_list))):
+        if t == '*' and t1 != '*':
+            trace_list[i] = t1
 
-        # if time-to-exceed error message or echo-reply message received, the console output on 2nd line(output_list[1])
-        # always have IP address of the responding hop
-        raw_output = subprocess.Popen(['ping', '-c', '1', '-m', str(i), '-i', '0.1', '-t', '1', ip],
-                                      stdout=subprocess.PIPE)
-        output = raw_output.communicate()[0].decode()
-        output_list = output.split('\n')
-        try:
+    # Concurrent processing of whois for both merit and cymru
+    for i in trace_list:
+        if i != '*':
+            merit_raw.append(subprocess.Popen(['whois', '-h', '198.108.0.18', i], stdout=subprocess.PIPE))
+            cymru_raw.append(subprocess.Popen(['whois', '-h', 'v4.whois.cymru.com', i], stdout=subprocess.PIPE))
+        else:
+            merit_raw.append('*')
+            cymru_raw.append('*')
 
-            # capture the IP address of the responding hop by reg
-            hop = re.search(r'(\d+\.\d+\.\d+\.\d+)', output_list[1]).group(1)
+    # Filter output and print result
+    for t, m, c in zip(trace_list, merit_raw, cymru_raw):
+        if m != '*':
+            merit, asn = merit_filter(m.communicate()[0].decode())
+            cymru = cymru_filter(c.communicate()[0].decode())
 
-        # if not replies from certain hop, then output_list[1] doesn't have any ip address to trigger AttributeError
-        except AttributeError:
+            # Due to ipip.net has concurrent limitation restriction, need to avoid con-current processing
+            ipip = search_as_name_ipip(t)
+
+            # To calculate the cross AS border and print '======AS BORDER======'
+            last_asn = as_border_check(asn, last_asn, file_name)
+
+            # print result
+            print_save(t, asn, merit, cymru, ipip, file_name, ip)
+        else:
             print_save('*', '*', '*', '*', '*', file_name, ip)
-
-        # To search merit and cymru's whois server, REST API to search ipip's ASN database
-        merit, asn = search_as_name_merit(hop)
-        cymru = search_as_name_cymru(hop)
-        ipip = search_as_name_ipip(hop)
-
-        # To calculate the cross AS border and print '======AS BORDER======'
-        last_asn = as_border_check(asn, last_asn)
-
-        # Print the result and cut only the first 30 characters for merit and cymru
-        print_save(hop, asn, merit, cymru, ipip, file_name, ip)
-
-        # if echo-reply message received, break out the loop
-        if '1 packets received' in output:
-            break
 
 
 if __name__ == '__main__':
